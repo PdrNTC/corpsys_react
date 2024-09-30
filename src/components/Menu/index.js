4c8f732b1bae90993a0b20384dac773d21a7e23a
Pedro Henrique Ernandes da Silva <thepedrinhoo@gmail.com>
2024-09-30 14:45:26 -0300

    estilizacao do menu principal e logo da corpsys

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,23 +1,46 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import logo from "../../imgs/logo_corpsys.png";
+
 
 const MenuContainer = styled.div`
+  background-color: #1a1a1a;
+  color: #fff;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background-color: #f0f0f0;
+  padding-top: 50px;
 `;
 
-const TituloMenu = styled.h2`
-    font-size: 32px;
+const Logo = styled.img`
+  width: 300px;
+  height: auto;
+  margin-bottom: 20px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #1a1a1a;
 `;
 
 const MenuItem = styled.div`
   position: relative;
-  margin: 10px;
+  margin: 0 15px;
+  padding: 10px 15px;
+  cursor: pointer;
+  font-size: 18px;
+  transition: color 0.3s;
+  &:hover {
+    color: #ff5252;
+  }
+
+  &:hover > div {
+    display: block;
+  }
 `;
 
 const SubMenu = styled.div`
@@ -25,23 +48,19 @@ const SubMenu = styled.div`
   position: absolute;
   top: 100%;
   left: 0;
-  background-color: #fff;
+  background-color: #333;
   box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
   z-index: 1;
-  ${MenuItem}:hover & {
-    display: block;
-  }
 `;
 
-const SubMenuItem = styled.button`
-  padding: 10px;
-  width: 150px;
-  text-align: left;
-  border: none;
-  background-color: #3A6D8C;
-  cursor: pointer;
+const SubMenuItem = styled.div`
+  font-weight: 700;
+  padding: 10px 15px;
+  color: #fff;
+  white-space: nowrap;
+  transition: background-color 0.3s;
   &:hover {
-    background-color: #e0e0e0;
+    background-color: #ff5252;
   }
 `;
 
@@ -50,38 +69,40 @@ function Menu() {
 
     return (
         <MenuContainer>
-            <TituloMenu>Menu</TituloMenu>
-            <MenuItem>
-                <SubMenuItem onClick={() => navigate('/cadastrar-cliente')}>Cliente</SubMenuItem>
-                <SubMenu>
-                    <SubMenuItem onClick={() => navigate('/exibir-clientes')}>Exibir Clientes</SubMenuItem>
-                    <SubMenuItem onClick={() => navigate('/cadastrar-cliente')}>Cadastrar Cliente</SubMenuItem>
-                </SubMenu>
-            </MenuItem>
+            <Logo src={logo} alt="logo da corpsystem" />
+            <Nav> 
                 <MenuItem>
-                    <SubMenuItem onClick={() => navigate('/cadastrar-vendedor')}>Vendedor</SubMenuItem>
+                    <SubMenuItem onClick={() => navigate('/cadastrar-cliente')}>Cliente</SubMenuItem>
+                    <SubMenu>
+                        <SubMenuItem onClick={() => navigate('/exibir-clientes')}>Exibir Clientes</SubMenuItem>
+                        <SubMenuItem onClick={() => navigate('/cadastrar-cliente')}>Cadastrar Cliente</SubMenuItem>
+                    </SubMenu>
+                </MenuItem>
+                    <MenuItem>
+                        <SubMenuItem onClick={() => navigate('/cadastrar-vendedor')}>Vendedor</SubMenuItem>
+                    <SubMenu>
+                        <SubMenuItem onClick={() => navigate('/exibir-vendedores')}>Exibir Vendedores</SubMenuItem>
+                        <SubMenuItem onClick={() => navigate('/cadastrar-vendedor')}>Cadastrar Vendedor</SubMenuItem>
+                    </SubMenu>
+                </MenuItem>
+                <MenuItem>
+                    <SubMenuItem onClick={() => navigate('/cadastrar-grupo-produto')}>Grupo de Produto</SubMenuItem>
                 <SubMenu>
-                    <SubMenuItem onClick={() => navigate('/exibir-vendedores')}>Exibir Vendedores</SubMenuItem>
-                    <SubMenuItem onClick={() => navigate('/cadastrar-vendedor')}>Cadastrar Vendedor</SubMenuItem>
+                    <SubMenuItem onClick={() => navigate('/exibir-grupo-produtos')}>Exibir Grupos</SubMenuItem>
+                    <SubMenuItem onClick={() => navigate('/cadastrar-grupo-produto')}>Cadastrar Grupo</SubMenuItem>
                 </SubMenu>
-            </MenuItem>
-            <MenuItem>
-                <SubMenuItem onClick={() => navigate('/cadastrar-grupo-produto')}>Grupo de Produto</SubMenuItem>
-            <SubMenu>
-                <SubMenuItem onClick={() => navigate('/exibir-grupo-produtos')}>Exibir Grupos</SubMenuItem>
-                <SubMenuItem onClick={() => navigate('/cadastrar-grupo-produto')}>Cadastrar Grupo</SubMenuItem>
-            </SubMenu>
-            </MenuItem>
-            <MenuItem>
-                <SubMenuItem onClick={() => navigate('/cadastrar-produto')}>Produto</SubMenuItem>
-            <SubMenu>
-                <SubMenuItem onClick={() => navigate('/exibir-produtos')}>Exibir Produtos</SubMenuItem>
-                <SubMenuItem onClick={() => navigate('/cadastrar-produto')}>Cadastrar Produto</SubMenuItem>
-            </SubMenu>
-            </MenuItem>
-            <MenuItem>
-                <SubMenuItem onClick={() => navigate('/exibir-relatorio')}>Exibir Relatório</SubMenuItem>
-            </MenuItem>
+                </MenuItem>
+                <MenuItem>
+                    <SubMenuItem onClick={() => navigate('/cadastrar-produto')}>Produto</SubMenuItem>
+                <SubMenu>
+                    <SubMenuItem onClick={() => navigate('/exibir-produtos')}>Exibir Produtos</SubMenuItem>
+                    <SubMenuItem onClick={() => navigate('/cadastrar-produto')}>Cadastrar Produto</SubMenuItem>
+                </SubMenu>
+                </MenuItem>
+                <MenuItem>
+                    <SubMenuItem onClick={() => navigate('/exibir-relatorio')}>Exibir Relatório</SubMenuItem>
+                </MenuItem>
+            </Nav>
         </MenuContainer>
     );
 }
